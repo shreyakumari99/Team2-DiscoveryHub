be9ce2d7cee8bd48757262899922beeba448e5dd
DevangChauhanR10 <www.devangchauhan3333@gmail.com>
2026-09-10 03:09:29 +0530

    Update evidence repository

--- services/case-service/src/main/java/com/smarsh/discoveryhub/cases/domain/EvidenceRepository.java
+++ services/case-service/src/main/java/com/smarsh/discoveryhub/cases/domain/EvidenceRepository.java
@@ -8,4 +8,7 @@ import java.util.List;
 @Repository
 public interface EvidenceRepository extends JpaRepository<EvidenceItem, String> {
     List<EvidenceItem> findByCaseId(String caseId);
+
+    /** Guards the (caseId, messageId) unique constraint before an insert is queued. */
+    boolean existsByCaseIdAndMessageId(String caseId, String messageId);
 }
