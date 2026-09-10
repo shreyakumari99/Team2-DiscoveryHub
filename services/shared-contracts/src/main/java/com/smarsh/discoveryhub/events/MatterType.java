4c8cd1f9f870160109667c7874809d01acfc674e
DevangChauhanR10 <www.devangchauhan3333@gmail.com>
2026-09-10 18:39:41 +0530

    Add matter type

---
+++ services/shared-contracts/src/main/java/com/smarsh/discoveryhub/events/MatterType.java
@@ -0,0 +1,10 @@
+package com.smarsh.discoveryhub.events;
+
+/**
+ * The kind of legal matter a case represents (FR-2.1).
+ */
+public enum MatterType {
+    INVESTIGATION,
+    LITIGATION,
+    REGULATORY_INQUIRY
+}
